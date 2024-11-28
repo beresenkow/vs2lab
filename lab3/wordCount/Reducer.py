16408b5ed5957fee1aa461897156e06772d999db
Erik Beresenkow <erik.beresenkow2@gmail.com>
2024-11-28 13:13:39 +0000

    Es sollte nun fertig sein.

--- lab3/wordCount/Reducer.py
+++ lab3/wordCount/Reducer.py
@@ -6,3 +6,18 @@ import constPipe as const
 class Reducer(threading.Thread):
     def __init__(self):
         threading.__init__(self)
+
+    def run(self):
+        context = zmq.Context()
+
+        mapperSocket = context.socket(zmq.PULL)
+        mapperSocket.bind("tcp://*" + self.port)
+
+        while True:
+            word = mapperSocket.recv().decode('UTF-8')#
+            if word in self.words:
+                self.words[word] += 1
+            else:
+                self.words[word] = 1
+
+            print("Das Wort: " + word + " wurde empfangen und zwar ganze " + str(self.words[word]) + " mal.")
