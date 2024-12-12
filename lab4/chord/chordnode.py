70e7b6c5c39bc35cea565ad7ae19c2885f7b9f5a
cgerl <cgerlach2003@gmail.com>
2024-12-12 13:17:15 +0000

    ?

--- lab4/chord/chordnode.py
+++ lab4/chord/chordnode.py
@@ -166,11 +166,6 @@ class ChordNode:
                     self.channel.send_to([request[2]], (constChord.LOOKUP_REP, next_id))
                 else:
                     self.channel.send_to([str(next_id)], (constChord.LOOKUP_REQ, key,current))
-            
-
-                # look up and return local successor 
-                #next_id: int = self.local_successor_node(request[1])
-                #self.channel.send_to([sender], (constChord.LOOKUP_REP, next_id))
 
                 ###newend
 
