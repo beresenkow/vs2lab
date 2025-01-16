8955b553e3628b1fa0571d65452b7c9ec55ade06
Erik Beresenkow <erik.beresenkow2@gmail.com>
2025-01-16 12:39:03 +0000

    Aufgabe 6 initialisiert

---
+++ lab6/3pc/const3PC.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+# coordinator messages
+VOTE_REQUEST = 'VOTE_REQUEST'
+PREPARE_COMMIT = "PREPARE_COMMIT"
+GLOBAL_COMMIT = 'GLOBAL_COMMIT'
+GLOBAL_ABORT = 'GLOBAL_ABORT'
+
+# participant messages
+VOTE_COMMIT = 'VOTE_COMMIT'
+VOTE_ABORT = 'VOTE_ABORT'
+READY_COMMIT = "READY_COMMIT"
+NEED_DECISION = 'NEED_DECISION'
+
+# participant decisions
+LOCAL_ABORT = 'LOCAL_ABORT'
+LOCAL_SUCCESS = 'LOCAL_SUCCESS'
+
+# fail-noisy crash timeout
+TIMEOUT = 1
+
+class CoordinatorState(Enum):
+    INIT = "INIT"
+    WAIT = "WAIT"
+    PRECOMMIT = "PRECOMMIT"
+    ABORT = "ABORT"
+    COMMIT = "COMMIT"
+
